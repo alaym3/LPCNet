e6cbc92aa1b27e87f922340384fef0d4d926dce1
AmL-Dev <61467804+AmL-Dev@users.noreply.github.com>
2023-01-03 23:11:13 +0100

    Update noise_data_preparation.sh

--- data/noise_data_preparation.sh
+++ data/noise_data_preparation.sh
@@ -1,34 +1,68 @@
+#!/bin/bash
+
 #### Make the required directories
-rm -rf clean_fullband/Normalized
-rm -rf clean_fullband/Volume_regulated
-rm -rf clean_fullband/combined.wav
-rm -rf clean_combined_16khz.wav
-mkdir clean_fullband/Normalized
-mkdir clean_fullband/Volume_regulated
+rm -rf noise_fullband/Normalized
+rm -rf noise_fullband/Volume_regulated
+rm -rf noise_fullband/combined.wav
+rm -rf noise_combined_16khz.wav
+mkdir noise_fullband/Normalized
+mkdir noise_fullband/Volume_regulated
 
 #### Normalize audio files
-for file in clean_fullband/*.wav;
+for file in noise_fullband/*.wav;
 do
     name=${file##*/}
     base=${name%.txt}
-    ffmpeg -i $file -filter:a loudnorm clean_fullband/Normalized/"${base}_norm.wav";
-done
+    ffmpeg -i $file -filter:a loudnorm noise_fullband/Normalized/"${base}_norm.wav";
+done;
 
+counter=0
+test=0
 #### Divide the loudness by 2
-for file in clean_fullband/Normalized/*.wav ;
+for file in noise_fullband/Normalized/*.wav ;
 do
-    name=${file##*/}
-    base=${name%.txt}
-    ffmpeg -i $file -filter:a "volume=0.5" clean_fullband/Volume_regulated/"${base}_reg.wav";
+    modulus=$(($counter % 100))
+    if (($modulus < 5));
+    then
+        name=${file##*/}
+        base=${name%.txt}
+        ffmpeg -i $file -filter:a "volume=0.0" noise_fullband/Volume_regulated/"${base}_reg.wav"
+
+    elif (($modulus < 40));
+    then
+        name=${file##*/}
+        base=${name%.txt}
+        ffmpeg -i $file -filter:a "volume=0.2" noise_fullband/Volume_regulated/"${base}_reg.wav"
+    elif (($modulus < 60));
+    then
+        name=${file##*/}
+        base=${name%.txt}
+        ffmpeg -i $file -filter:a "volume=0.4" noise_fullband/Volume_regulated/"${base}_reg.wav"
+    elif (($modulus < 85));
+    then
+       name=${file##*/}
+       base=${name%.txt}
+       ffmpeg -i $file -filter:a "volume=0.8" noise_fullband/Volume_regulated/"${base}_reg.wav"
+    elif (($modulus < 95));
+    then
+        name=${file##*/}
+        base=${name%.txt}
+        ffmpeg -i $file -filter:a "volume=1" noise_fullband/Volume_regulated/"${base}_reg.wav"
+    else
+        name=${file##*/}
+        base=${name%.txt}
+        ffmpeg -i $file -filter:a "volume=2" noise_fullband/Volume_regulated/"${base}_reg.wav"
+    fi
+    let counter++
 done;
 
 ### Combine all audio files into one
-sox clean_fullband/Normalized/*.wav clean_fullband/combined.wav
+sox noise_fullband/Volume_regulated/*.wav noise_fullband/combined.wav
 
 ### Change to mono and 16khz
-ffmpeg -i clean_fullband/combined.wav -ac 1 -ar 16000 clean_combined_16khz.wav
+ffmpeg -i noise_fullband/combined.wav -ac 1 -ar 16000 noise_combined_16khz.wav
 
 ### Make some space by deleting the temporary files
-rm -rf clean_fullband/Normalized
-rm -rf clean_fullband/Volume_regulated
-rm -rf clean_fullband/combined.wav
+rm -rf noise_fullband/Normalized
+rm -rf noise_fullband/Volume_regulated
+rm -rf noise_fullband/combined.wav
